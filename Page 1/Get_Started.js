1e347ed7fdc8cd451800a285121f12a8aa3dc32d
Wallace <lee_jia_hang_wallace@s2025.ssts.edu.sg>
2026-05-11 16:26:40 +0800

    completed show passwork for sign in page and reordered files

---
+++ Page 1/Get_Started.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let showPwButton = document.getElementById('show-pw')
+    let eyeIcon = document.getElementById('eye-icon')
+    let passwordField = document.getElementsByClassName('password-text')[0]
+    showPwButton.onclick = () => {
+        if (passwordField.type === 'password') {
+            eyeIcon.src = "/images/eye-open.png"
+            passwordField.type = "text"
+        } else {
+            eyeIcon.src = "/images/eye-close.png"
+            passwordField.type = "password"
+        }
+    }
+})
